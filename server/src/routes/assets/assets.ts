e8a12e1d8e05c25d887f698dc95935da7d11f0e0
VandalSausage <trumanwright13@gmail.com>
2025-03-18 09:15:13 +0000

    create the assets route and add all the logic for uploading to teh db there

---
+++ server/src/routes/assets/assets.ts
@@ -0,0 +1,81 @@
+import express from "express";
+import { query } from "../../conn";
+
+import multer from "multer";
+import {
+  getAssets,
+  getUploadAssetsStatement,
+  getJSONArray,
+  getCSVData,
+} from "./helpers";
+
+export const assets = express.Router();
+// to keep this simple I'm putting the entire file in memory
+const storage = multer.memoryStorage();
+// set file size limit at 10kb
+const fileLimit = 1024 * 10;
+const upload = multer({ storage, limits: { fileSize: fileLimit } });
+
+assets.get("/", async (request, response) => {
+  try {
+    // filter by client id
+    console.log(request.headers);
+    console.log(request.headers);
+    const clientId = request.query["Client-Id"] as string;
+    response.appendHeader("Content-Type", "application/json");
+    const resp = await query(getAssets(clientId));
+    response.status(200).json(resp);
+  } catch (e) {
+    console.error(e);
+    response.status(500).json({
+      message: "something went wrong",
+    });
+  }
+});
+
+assets.post("/upload", upload.single("assets"), async (request, response) => {
+  response.appendHeader("Content-Type", "application/json");
+  try {
+    if (!request.body.client_id) {
+      response.status(400).json({
+        message: "missing client id",
+      });
+    } else if (!request.file) {
+      response.status(400).json({
+        message: "missing upload file",
+      });
+    } else if (
+      request.file.mimetype !== "application/json" &&
+      request.file.mimetype !== "text/csv"
+    ) {
+      response.status(400).json({ message: "unknown file format" });
+    } else {
+      let statement;
+      switch (request.file.mimetype) {
+        case "application/json":
+          statement = getUploadAssetsStatement(
+            getJSONArray(request.file.buffer),
+            request.body.client_id
+          );
+          break;
+        case "text/csv":
+          statement = getUploadAssetsStatement(
+            getCSVData(request.file.buffer),
+            request.body.client_id
+          );
+      }
+
+      const resp = await query((db) => {
+        const { changes } = db.prepare(statement).run();
+        return changes;
+      });
+
+      response.status(200).json({ message: `assets created: ${resp}` });
+    }
+  } catch (e) {
+    console.error(e);
+    response.status(500).json({
+      message: "something went wrong",
+    });
+  }
+});
---
+++ server/src/routes/assets/helpers.ts
@@ -0,0 +1,119 @@
+import { v4 as uuidv4 } from "uuid";
+import papa from "papaparse";
+import { PoolConnection } from "better-sqlite-pool";
+
+// TODO create a shared types package
+export interface Asset {
+  id: string;
+  client_id: string;
+  latitude: number;
+  longitude: number;
+  address: string;
+}
+
+const getNumber = (x: unknown): number => {
+  if (typeof x === "number") {
+    return x;
+  } else {
+    throw new Error("data type mismatch");
+  }
+};
+
+const getString = (x: unknown): string => {
+  if (typeof x === "string") {
+    return x;
+  } else {
+    throw new Error("data type mismatch");
+  }
+};
+
+const isValidObject = (unsafeAsset: unknown): boolean =>
+  typeof unsafeAsset === "object" &&
+  !Array.isArray(unsafeAsset) &&
+  unsafeAsset !== null;
+
+const getSafeAsset = (unsafeAsset: unknown, clientId: string): Asset => {
+  // check it's a valid object
+  if (!isValidObject(unsafeAsset)) {
+    throw new Error("data format error");
+  }
+
+  const latitude = getNumber(unsafeAsset["latitude"]);
+  const longitude = getNumber(unsafeAsset["longitude"]);
+  const address = getString(unsafeAsset["address"]);
+
+  return {
+    latitude,
+    longitude,
+    address,
+    id: uuidv4(),
+    client_id: clientId,
+  };
+};
+
+const getAssetStatementBlock = (
+  { id, client_id, latitude, longitude, address }: Asset,
+  isFirst: boolean
+): string =>
+  (isFirst ? " " : " ,") +
+  `('${id}', '${client_id}', '${latitude}', '${longitude}', '${address}')`;
+
+export const getUploadAssetsStatement = (
+  json: unknown[],
+  clientId: string
+): string => {
+  let statement =
+    "INSERT INTO assets (id, client_id, latitude, longitude, address) VALUES";
+
+  // iterate through the json validating each row then transforming into the right sql and adding tothe statement
+  json.forEach((unsafeAsset, i) => {
+    statement += getAssetStatementBlock(
+      getSafeAsset(unsafeAsset, clientId),
+      i === 0
+    );
+  });
+
+  statement += ";";
+  return statement;
+};
+
+export const getAssets = (clientId?: string) => {
+  const statement = clientId
+    ? `SELECT * FROM assets WHERE client_id = '${clientId}'`
+    : "SELECT * FROM assets";
+
+  console.log({ clientId, statement });
+  return (db: PoolConnection): Asset[] =>
+    db.prepare(statement).all() as Asset[];
+};
+
+const isValidJSONArray = (array) => Array.isArray(array) && !!array.length;
+
+export const getJSONArray = (file: Buffer) => {
+  let array;
+  try {
+    array = JSON.parse(file.toString());
+
+    if (isValidJSONArray(array)) {
+      return array;
+    } else {
+      throw new Error();
+    }
+  } catch (e) {
+    console.log(e);
+    throw new Error("invalid json");
+  }
+};
+
+export const getCSVData = (file: Buffer) => {
+  const { data: array } = papa.parse(file.toString(), {
+    dynamicTyping: true,
+    header: true,
+  });
+
+  if (isValidJSONArray(array)) {
+    return array;
+  } else {
+    throw new Error("invalid json");
+  }
+};
